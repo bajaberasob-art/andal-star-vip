f57364a2860a28b93ed84536e386e163f84a109a
Abdullah <bajaberasob@gmail.com>
2026-05-19 02:57:10 +0300

    تفعيل اتصال Pooler المحصن لـ Render

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ app = Flask(__name__)
 
 # ================== إعدادات التطبيق والبروكسي ==================
 app.config['SECRET_KEY'] = 'abdullah-voucher-system-2026-final'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bajaberasobbaj72@db.hlepldoaxayyazklvthv.supabase.co:5432/postgres?sslmode=require'
+# لاحظ تغيير اسم المستخدم ليتضمن معرف المشروع (ضروري للـ Pooler)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.hlepldoaxayyazklvthv:bajaberasobbaj72@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres?sslmode=require'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # إخبار Flask بالوثوق بـ Cloudflare والأنفاق (لحل مشكلة HTTP/HTTPS)
